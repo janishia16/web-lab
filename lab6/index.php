3523995a321d447c3a75f7d9a7273753ffe36dec
janishia16 <92728653+janishia16@users.noreply.github.com>
2022-07-18 20:49:24 +0530

    Add files via upload

---
+++ lab6/index.php
@@ -0,0 +1,1 @@
+<?phpprint "<h3> REFRESH PAGE </h3>";$name="6.html";$file = fopen($name,"r");$hits= fscanf($file,"%d");fclose($file);$hits[0]++;$file = fopen($name,"w");fprintf($file,"%d",$hits[0]);fclose($file);print "Total number of views: ".$hits[0];?>
